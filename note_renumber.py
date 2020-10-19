1c14aac5c62ceea3c5b5b114bbe8a8e255fca4cd
David Grigg <git.user@rightword.com.au>
2020-10-19 13:48:21 +1100

    Allow endnotes within endnotes.xhtml

--- note_renumber.py
+++ note_renumber.py
@@ -65,13 +65,14 @@ def extract_anchor(href: str) -> str:
 # global variable, unfortunately
 notes_changed = 0
 
-def process_file(text_path: str, file_name: str, endnotes: list, current_note_number: int) -> int:
+def process_file(text_path: str, file_name: str, endnotes: list, de_orphan: bool, current_note_number: int) -> int:
 	"""
 	Reads a content file, locates and processes the endnotes,
 	accumulating info on them in a global list, and returns the next note number
 	:param text_path: path to the text files in the project
 	:param file_name: the name of the file being processed eg chapter-1.xhtml
 	:param endnotes: list of notes we are building
+	:param de_orphan: remove reference in text if no matching endnote
 	:param current_note_number: the current note number we are allocating
 	:return: the next note number to use
 	"""
@@ -101,6 +102,10 @@ def process_file(text_path: str, file_name: str, endnotes: list, current_note_nu
 			matches = list(filter(lambda x: x.anchor == old_anchor, endnotes))
 			if len(matches) == 0:
 				print("Couldn't find endnote with anchor " + old_anchor)
+				if de_orphan:
+					print("Removing orphan note ref in text")
+					link.clear()
+					needs_rewrite = True
 			elif len(matches) > 1:
 				print("Duplicate anchors in endnotes file for anchor " + old_anchor)
 			else:  # found a single match, which is what we want
@@ -115,7 +120,58 @@ def process_file(text_path: str, file_name: str, endnotes: list, current_note_nu
 	if needs_rewrite:
 		new_file = open(file_path, "w")
 		new_file.write(format_xhtml(str(soup)))
+		# new_file.write(str(soup))
 		new_file.close()
+	return current_note_number
+
+
+def process_endnotes_file(endnotes: list, de_orphan: bool, current_note_number: int) -> int:
+	"""
+	Reads the endnotes in memory, looking for note links WITHIN the body of notes
+	:param endnotes: list of notes we are building
+	:param de_orphan: remove reference in text if no matching endnote
+	:param current_note_number: the current note number we are allocating
+	:return: the next note number to use
+	"""
+	global notes_changed
+	for endnote in endnotes:
+		for content in endnote.contents:
+			if isinstance(content, Tag):
+				links = content.find_all("a")
+				for link in links:
+					epub_type = link.get("epub:type") or ""
+					if epub_type == "noteref":
+						old_anchor = ""
+						href = link.get("href") or ""
+						if href:
+							old_anchor = extract_anchor(href)
+						new_anchor = "note-{:d}".format(current_note_number)
+						if new_anchor != old_anchor:
+							print("Changed " + old_anchor + " to " + new_anchor + " in endnotes.xhtml")
+							notes_changed += 1
+							# update the link in the soup object
+							link["href"] = 'endnotes.xhtml#' + new_anchor
+							link["id"] = 'noteref-{:d}'.format(current_note_number)
+							link.string = str(current_note_number)
+							needs_rewrite = True
+							# now try to find this in existing endnotes
+							matches = list(filter(lambda x: x.anchor == old_anchor, endnotes))
+							if len(matches) == 0:
+								print("Couldn't find endnote with anchor " + old_anchor)
+								if de_orphan:
+									print("Removing orphan note ref in text")
+									link.clear()
+									needs_rewrite = True
+							elif len(matches) > 1:
+								print("Duplicate anchors in endnotes file for anchor " + old_anchor)
+							else:  # found a single match, which is what we want
+								listnote = matches[0]
+								listnote.number = current_note_number
+								listnote.matched = True
+								# we don't change the anchor or the back ref just yet
+								listnote.source_file = "endnotes.xhtml"
+								listnote.anchor = new_anchor
+							current_note_number += 1
 	return current_note_number
 
 
@@ -138,7 +194,7 @@ def get_notes(endnotes_soup: BeautifulSoup) -> list:
 				links = content.find_all("a")
 				for link in links:
 					epub_type = link.get("epub:type") or ""
-					if epub_type == "se:referrer":
+					if epub_type == "se:referrer" or epub_type == "backlink":
 						href = link.get("href") or ""
 						if href:
 							note.back_link = href
@@ -158,6 +214,7 @@ def recreate(textpath: str, notes_soup: BeautifulSoup, endnotes: list):
 	"""
 	ol = notes_soup.ol
 	ol.clear()
+	endnotes.sort(key=lambda enote: enote.number)
 	for endnote in endnotes:
 		if endnote.matched:
 			li = notes_soup.new_tag("li")
@@ -168,14 +225,15 @@ def recreate(textpath: str, notes_soup: BeautifulSoup, endnotes: list):
 					links = content.find_all("a")
 					for link in links:
 						epub_type = link.get("epub:type") or ""
-						if epub_type == "se:referrer":
+						if "se:referrer" in epub_type or "backlink" in epub_type:
 							href = link.get("href") or ""
 							if href:
 								link["href"] = endnote.source_file + "#noteref-" + str(endnote.number)
 				li.append(content)
 			ol.append(li)
-	new_file = open(os.path.join(textpath, "endnotes.xhtml"), "w")
+	new_file = open(os.path.join(textpath, "new_endnotes.xhtml"), "w")
 	new_file.write(format_xhtml(str(notes_soup)))
+	# new_file.write(str(notes_soup))
 	new_file.close()
 
 
@@ -185,6 +243,7 @@ exclude_list = ["titlepage.xhtml", "colophon.xhtml", "uncopyright.xhtml", "impri
 
 def main():
 	parser = argparse.ArgumentParser(description="Renumber endnotes from beginning")
+	parser.add_argument("-r", "--remove_orphans", action="store_true", help="remove notes in text if note is missing in endnotes.xhtml")
 	parser.add_argument("directory", metavar="DIRECTORY", help="a Standard Ebooks source directory")
 	args = parser.parse_args()
 
@@ -201,6 +260,11 @@ def main():
 		print("Error: no endnotes file exists")
 		exit(-1)
 
+	if args.remove_orphans:
+		de_orphan = True
+	else:
+		de_orphan = False
+
 	xhtml = gethtml(notespath)
 	notes_soup = BeautifulSoup(xhtml, "lxml")
 	endnotes = get_notes(notes_soup)
@@ -216,14 +280,17 @@ def main():
 			continue
 		print("Processing " + file_name)
 		processed += 1
-		current_num = process_file(textpath, file_name, endnotes, current_num)
+		current_num = process_file(textpath, file_name, endnotes, de_orphan, current_num)
 		print("Endnotes processed so far: " + str(current_num - 1))  # we subtract 1 because process_file increments it after each note found
+	process_endnotes_file(endnotes, de_orphan, current_num)
 	if processed == 0:
 		print("No files processed. Did you update manifest and order the spine?")
 	else:
 		print("Found {:d} endnotes.".format(current_num - 1))
+		# DEBUG
+		notes_changed = 1
 		if notes_changed > 0:
-			print("Changed {:d} endnotes")
+			print("Changed {:d} endnotes".format(notes_changed))
 			# so we need to recreate the endnotes file
 			recreate(textpath, notes_soup, endnotes)
 		else:
